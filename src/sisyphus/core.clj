d55e851cc480e1378a859b773b747769f25a58f4
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-10-19 10:43:34 +0530

    add `schedule-parser`

--- src/sisyphus/core.clj
+++ src/sisyphus/core.clj
@@ -19,3 +19,14 @@
    (* minutes 60)
    seconds))
 
+(defn- schedule-parser
+  "Parses a vector to return a period in seconds and a start time.
+  example `[:every 2 :days 5: hours 3 :minutes 2 :seconds :starting-at 5 :pm]`.
+  If the start time is not provided the task will immediately be run when `sisyphus/run` is called."
+  [schedule]
+  (let [days    (or (get-before #{:days :day} schedule) 0)
+        hours   (or (get-before #{:hours :hour} schedule)0)
+        minutes (or (get-before #{:minutes :minute} schedule) 0)
+        seconds (or (get-before #{:seconds :second} schedule) 0)
+        [_ start-time] (drop-while (complement #{:starting-at}) schedule)]
+     [(time->seconds days, hours, minutes, seconds)]))

3891acc5c6a02f8aa992b73cca454b86cc63ac12
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-10-19 09:12:42 +0530

    Initialize project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sisyphus
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/sisyphus/core_test.clj
@@ -0,0 +1,7 @@
+(ns sisyphus.core-test
+  (:require [clojure.test :refer :all]
+            [sisyphus.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

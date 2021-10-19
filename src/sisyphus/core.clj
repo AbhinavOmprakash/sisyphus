2741fc8db7b7b519ecf68ca14b1470a72d40d78d
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-10-19 13:25:45 +0530

    add `tasks` atom & fn `add-task`

--- src/sisyphus/core.clj
+++ src/sisyphus/core.clj
@@ -1,1 +1,13 @@
-(ns sisyphus.core)
+(ns sisyphus.core
+  (:require [sisyphus.utils :as utils]))
+
+(def ^:private tasks (atom {}))
+
+(defn add-task
+  [name task schedule]
+  (let [[interval start-time] (utils/schedule-parser schedule)
+        new-task {name {:task task
+                        :interval-in-seconds interval
+                        :due-at start-time}}]
+    ; should I throw an exception if the task already exists?
+    (swap! tasks conj new-task)))

78f1ca02d67d94cad0820ff16676957e7ad2275b
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-10-19 10:42:40 +0530

    add `get-before`

--- src/sisyphus/core.clj
+++ src/sisyphus/core.clj
@@ -1,6 +1,13 @@
 (ns sisyphus.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn- get-before
+  "Returns the element in coll before key-set.
+   Returns 0 if key not in coll."
+  [key-set coll]
+  (if (some key-set coll)
+    (reduce (fn [prev curr]
+              (if (key-set curr)
+                (reduced prev)
+                curr))
+            coll)
+    nil))

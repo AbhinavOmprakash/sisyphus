ffe02b6deafa861bd91ef56a39939495ae6fb6e1
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-10-19 16:44:37 +0530

    Change structure of new-task map
    - made it a flat map of 4 keywords.
    - use :name as keyword for name, instead of a string. Easier to query.

--- src/sisyphus/core.clj
+++ src/sisyphus/core.clj
@@ -6,8 +6,9 @@
 (defn add-task
   [name task schedule]
   (let [[interval start-time] (utils/schedule-parser schedule)
-        new-task {name {:task task
-                        :interval-in-seconds interval
-                        :due-at start-time}}]
+        new-task {:name                name
+                  :task                task
+                  :interval-in-seconds interval
+                  :due-at              start-time}]
     ; should I throw an exception if the task already exists?
     (swap! tasks conj new-task)))

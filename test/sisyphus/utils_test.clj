c5259c57f8e704755ba6c14205afe717dceb4ad4
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-10-20 15:43:14 +0530

    Fix tests

--- test/sisyphus/utils_test.clj
+++ test/sisyphus/utils_test.clj
@@ -3,28 +3,26 @@
             [sisyphus.utils :refer :all]))
 
 
-
-(def schedule-parser #'sisyphus.utils/schedule-parser)
-
 (deftest -schedule-parser
   (testing "all time periods get converted to seconds"
     (is (= (schedule-parser [:every 6 :days]) 
-           [(* 6 24 60 60)]))
+           [(* 6 24 60 60) nil]))
     (is (= (schedule-parser [:every 6 :hours]) 
-           [(* 6 60 60)]))
+           [(* 6 60 60) nil]))
     (is (= (schedule-parser [:every 6 :minutes]) 
-           [(* 6 60)]))
+           [(* 6 60) nil]))
     (is (= (schedule-parser [:every 6 :seconds]) 
-           [(* 6)])))
+           [(* 6) nil])))
   
   (testing "schedule can handle mixed time periods"
     (is (= (schedule-parser [:every 6 :days 6 :hours]) 
-           [(+ (* 6 24 60 60) (* 6 60 60))]))
+           [(+ (* 6 24 60 60) (* 6 60 60)) nil]))
     (is (= (schedule-parser [:every 6 :days 6 :hours 6 :minutes]) 
            [(+
              (* 6 24 60 60)
              (* 6 60 60)
-             (* 6 60))]))))
+             (* 6 60))
+            nil]))))
 
 
 

f2a57aaf387cddc948a6c65860c3fba197c0f1b8
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-10-20 16:41:48 +0530

    Add test for `add-tasks`

--- test/sisyphus/core_test.clj
+++ test/sisyphus/core_test.clj
@@ -1,2 +1,13 @@
 (ns sisyphus.core-test
-  (:require [clojure.test :refer :all]))
+  (:require [clojure.test :refer :all]
+            [sisyphus.core :refer :all]))
+
+(def tasks #'sisyphus.core/tasks)
+
+
+(deftest -add-tasks
+  (testing "that a task is properly added"
+    (let [test-fn #(println "test")]
+      (is (= (add-task! "print" test-fn [:every 1 :second])
+             [{:name "print" :task test-fn :interval-in-seconds 1 :due-at nil}]
+             @tasks)))))

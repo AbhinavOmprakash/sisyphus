1a12947b1cb868c44e50cd97f509b710e8f7fa81
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-10-21 11:15:10 +0530

    Lint

--- src/sisyphus/core.clj
+++ src/sisyphus/core.clj
@@ -89,10 +89,10 @@
    (add-task! name task schedule))
   ([name task schedule]
    (let [[interval start-time] (utils/schedule-parser schedule)
-         new-task {:name                name
-                   :task                task
-                   :interval-in-seconds interval
-                   :due-at              start-time}]
+         new-task              {:name                name
+                                :task                task
+                                :interval-in-seconds interval
+                                :due-at              start-time}]
      ; should I throw an exception if the task already exists?
      (swap! tasks conj new-task))))
 

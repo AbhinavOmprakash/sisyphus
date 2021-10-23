414cc6f7699ad3c41b22a039cca525c1b99b376e
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-10-23 20:48:42 +0530

    Add functions

--- src/sisyphus/log.clj
+++ src/sisyphus/log.clj
@@ -1,3 +1,19 @@
 (ns sisyphus.log
   (:require [java-time :as jtime]))
 
+(def ^:private log (atom (list)))
+
+(def ^:private lines-written-to-file (atom 0))
+
+(defn- get-lines-for-file []
+  (let [log-lines (count @log)
+        no-of-new-lines (- log-lines @lines-written-to-file)
+        new-lines (take no-of-new-lines @log)]
+    new-lines))
+
+(defn- update-lines-written-to-file! [lines]
+  (swap! lines-written-to-file + (count lines)))
+
+(defn- truncate [s n]
+  (subs s 0 n))
+

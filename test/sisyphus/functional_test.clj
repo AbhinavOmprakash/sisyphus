22bbd2d873934ff8a421a75d9e0a719422814484
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2021-10-24 19:59:55 +0530

    Changes

--- test/sisyphus/functional_test.clj
+++ test/sisyphus/functional_test.clj
@@ -6,17 +6,27 @@
          (fn []
            (with-open [w (clojure.java.io/writer  (str (rand-int 100) ".txt")  :append true)]
              (.write w (str "hello" "world"))))
-         [:every 2 :seconds]])
+         [:every 5 :seconds]])
 
 (def t2 ["writefileSleep"
          (fn []
            (with-open [w (clojure.java.io/writer  (str "sleep" (rand-int 500) ".txt")  :append true)]
              (.write w (str "hello" "world")))
            (Thread/sleep 10000))
-         [:every 2 :seconds]])
+         [:every 10 :seconds]])
 
-(sisy/add-task! t1)
+(def t-exception ["exception"
+                  (fn [] (throw RuntimeException))
+                  [:every 7 :seconds]])
 
-(sisy/add-task! t2)
+(apply sisy/add-task! t1)
+
+(apply sisy/add-task! t2)
+(apply sisy/add-task! t-exception)
+
 
 (sisy/run-tasks!)
+(Thread/sleep 60000)
+(sisy/stop-tasks!)
+
+

c2584ea020bb9dfa8cba79863b149cae2835a5a1
Abhinav Omprakash <55880260+AbhinavOmprakash@users.noreply.github.com>
2021-11-15 08:54:27 +0530

    Change License

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject org.clojars.abhinav/sisyphus "0.1.1"
   :description "Sisyphus is a task scheduler that runs clojure functions periodically."
   :url "https://github.com/AbhinavOmprakash/sisyphus"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "MIT"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [clojure.java-time "0.3.3"]]
   :plugins [[lein-auto "0.1.3"]]
